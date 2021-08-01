f1fdbffc1aa158fad091b25a9e652c6f6b96dbc0
Sal Ferrarello <sal@ironcodestudio.com>
2021-08-01 07:00:12 -0400

    Change heading to "mount vs render"

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          mount vs render
         </p>
       </header>
     </div>
