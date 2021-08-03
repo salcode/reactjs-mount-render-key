8031f43a4bc97e1bbba0c1ff9ea6dcf35f85e821
Sal Ferrarello <sal@ironcodestudio.com>
2021-08-03 15:17:27 -0400

    Add driver variable to display in Bumper Car

--- src/components/BumperCar/index.js
+++ src/components/BumperCar/index.js
@@ -1,8 +1,12 @@
 function BumperCar() {
+  const driver = 'sal';
   return (
     <div className="bumper-car">
       <strong>Bumper Car</strong>
       <span className="fade-out">(mount)</span>
+      <div className="bumper-car__driver">
+        {driver}
+      </div>
     </div>
   );
 }
