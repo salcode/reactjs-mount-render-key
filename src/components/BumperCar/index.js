a1ce31291c091adeb278a386af1b9c043e148aa9
Sal Ferrarello <sal@ironcodestudio.com>
2021-08-10 15:28:39 -0400

    Add whitespace to BumperCar component

--- src/components/BumperCar/index.js
+++ src/components/BumperCar/index.js
@@ -1,11 +1,15 @@
 function BumperCar({driver}) {
   return (
     <div className="bumper-car">
+
       <strong>Bumper Car</strong>
+
       <span className="fade-out">(mount)</span>
+
       <div className="bumper-car__driver">
         {driver}
       </div>
+
     </div>
   );
 }
