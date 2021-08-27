e5abcdc87e4e270b6070e11c4b5a10763097222f
Sal Ferrarello <sal@ironcodestudio.com>
2021-08-27 11:46:59 -0400

    After 2s change isMounted state to false
    After 2s change isMounted state to false in the Mounted component
    
    This will display our "(mounted)" message for 2 seconds and then remove
    it

--- src/components/Mounted/index.js
+++ src/components/Mounted/index.js
@@ -3,6 +3,13 @@ import { useEffect, useState } from 'react';
 export default function Mounted() {
   const [isMounted, setIsMounted] = useState(true);
 
+  useEffect(() => {
+    setTimeout(
+      () => setIsMounted(false),
+      2000
+    );
+  }, []);
+
   if (! isMounted) {
     return null;
   }
