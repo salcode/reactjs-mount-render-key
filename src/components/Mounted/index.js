ae377d1ea14495f2f734b45649fe8df55d533a08
Sal Ferrarello <sal@ironcodestudio.com>
2021-08-27 11:43:00 -0400

    Add isMounted state and conditionally display
    Add isMounted state and conditionally display the div based on the
    isMounted value

--- src/components/Mounted/index.js
+++ src/components/Mounted/index.js
@@ -1,4 +1,11 @@
+import { useEffect, useState } from 'react';
+
 export default function Mounted() {
+  const [isMounted, setIsMounted] = useState(true);
+
+  if (! isMounted) {
+    return null;
+  }
   return (
     <div
       className="mounted-indicator"
