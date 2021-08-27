8f755e49eee5f732d365425e414ec7969bc4b80e
Sal Ferrarello <sal@ironcodestudio.com>
2021-08-27 15:51:59 -0400

    Add control over keys used on statepage

--- src/components/StatePage/index.js
+++ src/components/StatePage/index.js
@@ -3,11 +3,12 @@ import BumperCar from '../StatefulBumperCar';
 
 export default function StatePage({initialCarsArray}) {
   const [cars, setCars] = useState(initialCarsArray);
+  const [keyType, setKeyType] = useState('index');
   return (
     <div>
       {
         cars.map((car, i) => {
-          const key = i;
+          const key = keyType === 'index' ? i : car.id;
           return <BumperCar
             color={car.color}
             key={key}
@@ -15,6 +16,16 @@ export default function StatePage({initialCarsArray}) {
           />
         })
       }
+      <br />
+      <select
+        onChange={(event) => {
+          setKeyType(event.target.value);
+        }}
+      >
+        <option value="index" selected>Use array index for key</option>
+        <option value="id">Use car.id for key</option>
+      </select>
+      <br />
       <br />
       <button
         onClick={() => setCars(
