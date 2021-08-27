e3e1aad1bf5598df6e9f3caf38601d52b9b9ddde
Sal Ferrarello <sal@ironcodestudio.com>
2021-08-27 14:02:20 -0400

    Add React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import BumperCar from './components/BumperCar';
+import NavBar from './components/NavBar';
+import PropsPage from './components/PropsPage';
+import StatePage from './components/StatePage';
 
 const drivers = [
   'abby',
@@ -37,61 +42,74 @@ function App() {
   const [hasColorAsKey, setHasColorAsKey] = useState(false);
   const [cars, setCars] = useState(originalCars);
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          mount vs render
-        </p>
-      </header>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <NavBar />
+        </header>
+        <main>
+          <Switch>
+            <Route
+              path="/props-page"
+              component={() => <PropsPage />}
+            />
+            <Route
+              path="/state-page"
+              component={() => <StatePage />}
+            />
+          </Switch>
 
-      {
-        cars.map((car, i) => (
-          <BumperCar
-            color={car.color}
-            driver={drivers[(driverIndex+i) % drivers.length] ?? '(empty)'}
-            key={hasColorAsKey ? car.color : i}
-          />
-        ))
-      }
 
-      <button
-        onClick={() => setDriverIndex(
-          (current) => ++current
-        )}
-      >
-        Next driver
-      </button>
-      <br />
-      <button
-        onClick={ () => setCars(
-          // (cars) => cars.reverse() // does NOT trigger re-render
-          // (cars) => [...cars] // DOES trigger re-render
-          (cars) => [...cars].reverse() // DOES trigger re-render
+        {
+          cars.map((car, i) => (
+            <BumperCar
+              color={car.color}
+              driver={drivers[(driverIndex+i) % drivers.length] ?? '(empty)'}
+              key={hasColorAsKey ? car.color : i}
+            />
+          ))
+        }
 
-          // (cars) => { // does NOT trigger re-render
-          //   cars.push({color: 'orange'});
-          //   return cars;
-          // }
-          // (cars) => { // DOES trigger re-render
-          //   const newCars = [...cars];
-          //   newCars.push({color: 'orange'});
-          //   return newCars;
-          // }
-          // (cars) => [...cars, {color: 'orange'}]; // DOES trigger re-render
-        ) }
-      >
-        Reverse Car Order
-      </button>
-      <br />
-      <div>
-        Key is <code>{hasColorAsKey ? "car.color" : "(array index)"}</code>
+        <button
+          onClick={() => setDriverIndex(
+            (current) => ++current
+          )}
+        >
+          Next driver
+        </button>
+        <br />
+        <button
+          onClick={ () => setCars(
+            // (cars) => cars.reverse() // does NOT trigger re-render
+            // (cars) => [...cars] // DOES trigger re-render
+            (cars) => [...cars].reverse() // DOES trigger re-render
+
+            // (cars) => { // does NOT trigger re-render
+            //   cars.push({color: 'orange'});
+            //   return cars;
+            // }
+            // (cars) => { // DOES trigger re-render
+            //   const newCars = [...cars];
+            //   newCars.push({color: 'orange'});
+            //   return newCars;
+            // }
+            // (cars) => [...cars, {color: 'orange'}]; // DOES trigger re-render
+          ) }
+        >
+          Reverse Car Order
+        </button>
+        <br />
+        <div>
+          Key is <code>{hasColorAsKey ? "car.color" : "(array index)"}</code>
+        </div>
+        <button
+          onClick={() => setHasColorAsKey((hasColorAsKey) => !hasColorAsKey)}
+        >
+          Toggle value used for Key
+        </button>
+        </main>
       </div>
-      <button
-        onClick={() => setHasColorAsKey((hasColorAsKey) => !hasColorAsKey)}
-      >
-        Toggle value used for Key
-      </button>
-    </div>
+    </BrowserRouter>
   );
 }
 
