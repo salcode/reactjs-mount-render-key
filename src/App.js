45c486773163a2bae60a531bb9d1d021fa66f2db
Sal Ferrarello <sal@ironcodestudio.com>
2021-08-27 15:25:25 -0400

    Remove unused code form App.js

--- src/App.js
+++ src/App.js
@@ -38,9 +38,6 @@ const originalCars = [
 ];
 
 function App() {
-  const [driverIndex, setDriverIndex] = useState(0);
-  const [hasColorAsKey, setHasColorAsKey] = useState(false);
-  const [cars, setCars] = useState(originalCars);
   return (
     <BrowserRouter>
       <div className="App">
@@ -63,55 +60,6 @@ function App() {
               />}
             />
           </Switch>
-
-
-        {
-          cars.map((car, i) => (
-            <BumperCar
-              color={car.color}
-              driver={drivers[(driverIndex+i) % drivers.length] ?? '(empty)'}
-              key={hasColorAsKey ? car.color : i}
-            />
-          ))
-        }
-
-        <button
-          onClick={() => setDriverIndex(
-            (current) => ++current
-          )}
-        >
-          Next driver
-        </button>
-        <br />
-        <button
-          onClick={ () => setCars(
-            // (cars) => cars.reverse() // does NOT trigger re-render
-            // (cars) => [...cars] // DOES trigger re-render
-            (cars) => [...cars].reverse() // DOES trigger re-render
-
-            // (cars) => { // does NOT trigger re-render
-            //   cars.push({color: 'orange'});
-            //   return cars;
-            // }
-            // (cars) => { // DOES trigger re-render
-            //   const newCars = [...cars];
-            //   newCars.push({color: 'orange'});
-            //   return newCars;
-            // }
-            // (cars) => [...cars, {color: 'orange'}]; // DOES trigger re-render
-          ) }
-        >
-          Reverse Car Order
-        </button>
-        <br />
-        <div>
-          Key is <code>{hasColorAsKey ? "car.color" : "(array index)"}</code>
-        </div>
-        <button
-          onClick={() => setHasColorAsKey((hasColorAsKey) => !hasColorAsKey)}
-        >
-          Toggle value used for Key
-        </button>
         </main>
       </div>
     </BrowserRouter>
