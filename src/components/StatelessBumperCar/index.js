449630e743013172cdebf33ea1a229bb26f348ee
Sal Ferrarello <sal@ironcodestudio.com>
2021-08-27 15:18:36 -0400

    Change styling of Bumper car component

--- src/components/StatelessBumperCar/index.js
+++ src/components/StatelessBumperCar/index.js
@@ -2,7 +2,7 @@ import Mounted from '../Mounted';
 
 export default function StatelessBumperCar({color, driver}) {
   return (
-    <div className="bumper-car" style={{borderColor: color}}>
+    <div className="bumper-car" style={{backgroundColor: color}}>
       <Mounted />
       <strong>Bumper Car</strong>
       <div className="bumper-car__driver">
